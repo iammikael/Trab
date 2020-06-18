71a7853b762b564f276bc5b7e3689e8d1ee86b20
iammikael <67081572+iammikael@users.noreply.github.com>
2020-06-17 20:01:37 -0500

    Add files via upload
    top

---
+++ Trabalho/exx.js
@@ -0,0 +1,56 @@
+function ver(){
+   document.getElementById('tela').className="invisivel";
+   document.getElementById('cab').className="visivel";
+   document.getElementById('principal').className="visivel";
+}
+function ver_if(){
+	document.getElementById('texto_if').className="visivel";
+	document.getElementById('principal').className="invisivel";
+	document.getElementById('texto_switch').className="invisivel";
+    document.getElementById('question').className="invisivel";
+}
+/*/
+function ver_else(){
+	document.getElementById('texto_else').className="visivel";
+	document.getElementById('principal').className="invisivel";
+	document.getElementById('texto_if').className="invisivel";
+	document.getElementById('texto_switch').className="invisivel";
+    document.getElementById('texto_case').className="invisivel";
+}
+/*/
+function ver_switch(){
+	document.getElementById('texto_switch').className="visivel";
+	document.getElementById('principal').className="invisivel";
+	document.getElementById('texto_if').className="invisivel";
+	document.getElementById('question').className="invisivel";
+}
+function ver_quest(){
+	document.getElementById('question').className="visivel";
+	document.getElementById('principal').className="invisivel";
+	document.getElementById('texto_if').className="invisivel";
+	document.getElementById('texto_switch').className="invisivel";
+}
+function voltar(){
+	document.getElementById('principal').className="visivel";
+	document.getElementById('texto_case').className="invisivel";
+	document.getElementById('texto_if').className="invisivel";
+	document.getElementById('texto_else').className="invisivel";
+	document.getElementById('texto_switch').className="invisivel";
+}
+function reset(){
+	document.getElementById('nome').value=' ';
+	document.getElementById('email').value=' ';
+	document.getElementById('duv').value= ' ';
+}
+function enviar(){
+	n=document.getElementById('nome').value;
+	e=document.getElementById('email').value;
+	d=document.getElementById('duv').value;
+
+	vet=[]
+
+	if(n=n.length){
+		vet=n;
+		alert("Informaçoes enviadas")
+	}
+}
